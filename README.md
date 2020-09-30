eb5d1fa2b7f6cb6cae266cb6540778d48cd7a26c
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-09-30 12:42:17 +0200

    Add class template

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # algorithm_092020_nom1_nom2
+
 Modèle du projet du cours d'algorithmes
+
+# Description
+
+Vous devez créer une Fibonnaci heap.
+C'est à dire une structure capable de stocker des nombres.
+
+# Documentation
+
+https://en.wikipedia.org/wiki/Heap_(data_structure)
+
+https://en.wikipedia.org/wiki/Fibonacci_heap
+
+L'implémentation est décrite en anglais : https://en.wikipedia.org/wiki/Fibonacci_heap#Implementation_of_operations
+et en français : https://fr.wikipedia.org/wiki/Tas_de_Fibonacci#Implémentation_des_opérations
---
+++ fibonnaci_heap.py
@@ -0,0 +1,41 @@
+class FibonnaciHeap(object):
+    """
+    Une fibonnaci heap est un arbre permettant de stocker et trier des donnés efficacement
+
+    https://en.wikipedia.org/wiki/Heap_(data_structure)
+
+    https://en.wikipedia.org/wiki/Fibonacci_heap
+
+    L'implémentation est décrite en anglais : https://en.wikipedia.org/wiki/Fibonacci_heap#Implementation_of_operations
+    et en français : https://fr.wikipedia.org/wiki/Tas_de_Fibonacci#Implémentation_des_opérations
+    """
+
+    def insert(self, value: int) -> None:
+        """
+        Ajoute une valeur dans l'arbre
+        """
+        pass
+
+    def find_min(self) -> int:
+        """
+        Retourne la valeur minimum dans l'arbre
+        """
+        pass
+
+    def delete_min(self) -> int:
+        """
+        Supprime et retourne la valeur minimum dans l'arbre
+        """
+        pass
+
+    def decrease_key(self, current_value: int, new_value :int) -> None:
+        """
+        Modify une valeur dans l'arbre
+        """
+        pass
+
+    def merge(self, fibonnaci_heap: object) -> None:
+        """
+        Fusionne deux arbres
+        """
+        pass
